c8b2174aef881ad8f1ba600b9d36e6e7066138a9
AlGray <allan.gray@sunetrike.com>
2017-07-12 14:10:43 +0800

    1. Added a new module, log_editor which will serve as the main terminal that a user will use to view previous data and add new entries to the log.  2. Also added the sample data that log_editor should be able to interact with.  3. Added a blank log.csv that will serve as a scratch pad for creating and manipulating dummy data

---
+++ log_editor.py
@@ -0,0 +1,8 @@
+
+# coding: utf-8
+
+
+# log_editor is used to take data from a csv and manipulate it:
+#	1. import the data and make a local copy
+#	2. display the last ten rows of the data
+# Please use "sample_log.csv" to implement this
